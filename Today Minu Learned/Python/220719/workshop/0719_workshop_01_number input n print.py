9b75a339583959f06d2f7a83400d8916c336c105
minu-j <minu.j.dev@gmail.com>
2022-07-19 09:56:15 +0900

    today workshop

---
+++ Today Minu Learned/Python/220719/workshop/0719_workshop_01_number input n print.py
@@ -0,0 +1,4 @@
+num_1 = int(input('please enter num 1 : '))
+num_2 = int(input('please enter num 2 : '))
+
+print(num_1 + num_2)
---
+++ Today Minu Learned/Python/220719/workshop/0719_workshop_02_average.py
@@ -0,0 +1,3 @@
+menu = {"짬뽕" : 9000, "돈까스" : 12000, "초밥" : 16000}
+
+print(int(sum(menu.values()))/len(menu.values()))
