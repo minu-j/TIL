5ba1e628d65d5db8e950f6565daf86ba2ea0cafe
minu-j <minu.j.dev@gmail.com>
2022-07-21 17:33:55 +0900

    추가 문제풀이

---
+++ Today Minu Learned/Python/220721/play/visited-list.py
@@ -0,0 +1,29 @@
+town = [0, 0, 0, 0, 0]
+while True:
+    print(f'현황 : {town}')
+    if town == [1, 1, 1, 1, 1]:
+         print('방문 배열 완성! : [1, 1, 1, 1, 1]')
+         break
+
+    else:
+        visit = int(input('방문할 인덱스를 고르세요 : '))
+
+        if visit >= 5:
+            print('==========')
+            print('다시 고르자')
+            print('==========')
+            pass
+
+        elif visit < 0:
+            print('==========')
+            print('다시 고르자')
+            print('==========')
+            pass
+
+        else:
+            if town[visit] == 0:
+                town[visit] = 1
+            elif town[visit] == 1:
+                pass
+        print(town)
+
