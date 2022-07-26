8a358abef86d7dfddadcd5257c1e94883559e133
minu-j <minu.j.dev@gmail.com>
2022-07-26 23:59:46 +0900

    swea 풀이

--- SWEA/220726/0726_D2_1959.py
+++ SWEA/220726/0726_D2_1959.py
@@ -1,21 +1,41 @@
 case = int(input())
+
 n = []
 m = []
 ai = []
 bj = []
 
-for i in range(case):
+for x in range(case):
     nm = list(input().split())
     n.append(int(nm[0]))
     m.append(int(nm[1]))
 
-    a = []
-    b = []
+    if n >= m:
+        ai.append(list(map(int, input().split())))
+        bj.append(list(map(int, input().split())))
+    elif m > n:
+        n, m = m, n
+        bj.append(list(map(int, input().split())))
+        ai.append(list(map(int, input().split())))
 
-    a = input().split()
-    b = input().split()
-
-    ai.append(a)
-    bj.append(b)
 
 print(n, m, ai, bj)
+
+for x in range(case):
+    max = 0
+    if n[x] == m[x]:
+        for y in range(n[x]):
+            print(ai[x][y] * bj[x][y])
+            if max < ai[x][y] * bj[x][y]:
+                max = ai[x][y] * bj[x][y]
+
+    else:
+        i = 0
+        for y in range(m[x]):
+            for z in range(n[x] - m[x] + 1):
+                print(ai[x][z], bj[x][y])
+                if max < ai[x][z + i] * bj[x][y]:
+                    max = ai[x][z + i] * bj[x][y]
+            i += 1
+
+    print(f'#{x + 1} {max}')        
