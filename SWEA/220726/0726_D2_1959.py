5090bb312b2483426b011e7864987e508e51a4ac
minu-j <minu.j.dev@gmail.com>
2022-07-28 00:00:21 +0900

    swea 문제풀이

--- SWEA/220726/0726_D2_1959.py
+++ SWEA/220726/0726_D2_1959.py
@@ -1,37 +1,30 @@
 case = int(input())
 
-n = []
-m = []
 ai = []
 bj = []
 
 for x in range(case):
-    nm = list(input().split())
+    n, m = map(int, input().split())
 
-    if nm[0] >= nm[1]:
-        n.append(int(nm[0]))
-        m.append(int(nm[1]))    
+    if n >= m:
         ai.append(list(map(int, input().split())))
         bj.append(list(map(int, input().split())))
 
-    elif nm[1] > nm[0]:
-        m.append(int(nm[0]))
-        n.append(int(nm[1]))    
+    elif m > n:
         bj.append(list(map(int, input().split())))
         ai.append(list(map(int, input().split())))
 
-for x in range(case):
-    max = 0
-    if n[x] == m[x]:
-        for y in range(n[x]):
-            if max < ai[x][y] * bj[x][y]:
-                max = ai[x][y] * bj[x][y]
-
-    else:
-        while len(ai[x]) >= len(bj[x]):
-            for y in range(len(bj[x])):
-                if max < ai[x][y] * bj[x][y]:
-                    max = ai[x][y] * bj[x][y]
-            bj[x].insert(0, 0)
+for _ in range(case):
+    max = -9999999
     
-    print(f'#{x + 1} {max}')        
+    while len(bj[_]) <= len(ai[_]):
+        mul = []
+        for i in range(len(bj[_])):
+            mul.append(bj[_][i] * ai[_][i])
+        
+        if sum(mul) > max:
+            max = sum(mul)
+
+        bj[_].insert(0, 0)
+    
+    print(f'#{_ + 1} {max}')   
