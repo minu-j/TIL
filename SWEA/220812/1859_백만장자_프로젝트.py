ab8042657d4e824d9bf0a3ecf6338ea6487ec622
minu-j <minu.j.dev@gmail.com>
2022-08-12 17:48:20 +0900

    0812 til

--- SWEA/220812/1859_백만장자_프로젝트.py
+++ SWEA/220812/1859_백만장자_프로젝트.py
@@ -8,19 +8,15 @@ for tc in range(T):
     end_day = N
     income = 0
     day_count = 0
+    last_price = price[end_day]
+    sum_price = []
 
-    while True:
-        max_price = 0
-        for i in range(start_day, end_day):
-            print(i)
-            if max_price < price[i]:
-                max_day = i
-                max_price = price[i]
+    for i in range(end_day, start_day, -1):
+        print(i)
+        if last_price >= price[i]:
+            sum_price.append(price[i])
+        elif last_price < price[i]:
 
-        income += max_price * max_day - sum(price[start_day:max_day])
-        print('상한가', max_day, max_price, income)
-        start_day = max_day + 1
-        if start_day == N:
-            break
+
 
     # print(f'#{tc + 1} {income}')
