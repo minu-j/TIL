8a7c2c33a5641685f3f61ab3aabb3e44c9a8d2b2
minu-j <minu.j.dev@gmail.com>
2022-08-23 17:29:45 +0900

    0823 til

--- SWEA/2806_N-Queen.py
+++ SWEA/2806_N-Queen.py
@@ -1,31 +1,30 @@
-di = [-1, -1, 0, 1, 1, 1, 0, -1]
-dj = [0, 1, 1, 1, 0, -1, -1, -1]
-
 def position_queen(n, depth):
     if depth == n:
+        queens.append(sel[::])
         return
-    for _ in range(depth, n):
-        
+    for _ in range(n):
+        if not check[_]:
+            sel[depth] = (depth, _)
+            check[_] = 1
+            position_queen(n, depth + 1)
+            check[_] = 0
+            sel[depth] = 0
 
 
 for tc in range(int(input())):
     N = int(input())
-    matrix = [[0] * (N + 2) for _ in range(N + 2)]
 
-    # 퀸이 들어갈 수 있는 위치 구하기
+    sel = [0] * N
+    check = [0] * N
+    queens = []
 
-    # 체스판 테두리에 벽 치기
-    for i in range(N + 2):
-        if i == 0 or i == N + 1:
-            for j in range(N + 2):
-                matrix[i][j] = 2
-        matrix[i][0] = 2
-        matrix[i][N + 1] = 2
+    position_queen(N, 0)
 
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
-            print(matrix[i][j])
+    di = [1, 1]
+    dj = [1, -1]
 
     ans = 0
+
+# 대각선 방향을 확인해야 합니다
 
     print(f'#{tc + 1} {ans}')
