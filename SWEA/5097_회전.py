ea0cc596063e70516466744869affad0ae682938
minu-j <minu.j.dev@gmail.com>
2022-08-25 10:26:09 +0900

    0825 til

--- SWEA/5097_회전.py
+++ SWEA/5097_회전.py
@@ -1,15 +1,13 @@
 # 데크로 풀기
-from collections import deque
-
-for tc in range(int(input())):
-    N, M = map(int, input().split())
-    queue = deque(map(int, input().split()))
-    for i in range(M):
-        now = queue.popleft()
-        queue.append(now)
-    print(f'#{tc + 1} {queue.popleft()}')
-
-#===================================
+# from collections import deque
+#
+# for tc in range(int(input())):
+#     N, M = map(int, input().split())
+#     queue = deque(map(int, input().split()))
+#     for i in range(M):
+#         now = queue.popleft()
+#         queue.append(now)
+#     print(f'#{tc + 1} {queue.popleft()}')
 
 # 다른 풀이 -> M을 N으로 나눈 나머지의 위치와 동일
 for tc in range(int(input())):
