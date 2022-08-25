a5d0687242b3c08bd3e626aa7dedae605a6650ff
minu-j <minu.j.dev@gmail.com>
2022-08-25 16:26:09 +0900

    0825 til

--- SWEA/2806_N-Queen.py
+++ SWEA/2806_N-Queen.py
@@ -1,32 +1,33 @@
-def position_queen(n, depth):
-    if depth == n:
-        queens.append(sel[::])
+# 퀸이 들어갈 수 있는 경우의 수를 구하는 함수
+def queen(n, depth):
+    global ans
+    if depth == n:      # 안겹치고 N까지 왔다면, 정답 +1
+        ans += 1
         return
-    for _ in range(n):
-        if not check[_]:
-            sel[depth] = (depth, _)
-            check[_] = 1
-            position_queen(n, depth + 1)
-            check[_] = 0
-            sel[depth] = 0
+    for now in range(n):        # 순열 재귀함수 응용
+        if not check[depth][now]:          # 해당 행에 퀸을 놓을 자리가 있다면
+            check_mark(n, depth, now, 1)    # 상하좌우, 대각선 +1을 하고
+            queen(n, depth + 1)             # 그 다음 행에 퀸을 놓으러 감
+            check_mark(n, depth, now, -1)   # 다녀왔다면 다시 상하좌우, 대각선 -1해주기
+
+
+# 체스판의 크기와 좌표를 입력하면 check리스트의 해당 좌표의 상하좌우, 대각선에 특정 값을 더해주는 함수
+def check_mark(n, x, y, mark):
+    check[x][y] = 1                     # 일단 해당 위치를 1로 만들고,
+    for i in range(n):
+        for j in range(n):
+            if x - i == y - j:          # 우하향하는 대각선에 +1
+                check[i][j] += mark
+            elif x + y == i + j:        # 우상향하는 대각선에 +1
+                check[i][j] += mark
+            elif x == i or y == j:      # 가로, 세로에도 +1
+                check[i][j] += mark
 
 
 for tc in range(int(input())):
     N = int(input())
-
     sel = [0] * N
-    check = [0] * N
-    queens = []
-
-    position_queen(N, 0)
-
-    di = [1, 1]
-    dj = [1, -1]
-
+    check = [[0] * N for _ in range(N)]   # 체크 배열을 2차원으로 만들기
     ans = 0
-
-    # 대각선 방향을 확인해야 합니다
-    for i in queens:
-
-
+    queen(N, 0)
     print(f'#{tc + 1} {ans}')
--- SWEA/test.py
+++ SWEA/test.py
@@ -1,18 +1,17 @@
-for tc in range(int(input())):
-    num = list(map(int, input().split()))
-    ans = 0
-
-    for i in range(1 << len(num)):
-
-        sum_num = 0
-
-        for j in range(len(num)):
-            if i & (1 << j):
-                sum_num += num[j]
-
-        if i > 0 and sum_num == 0:
-            ans = 1
-
-    print(f'#{tc + 1} {ans}')
+def search(n, x, y):
+    sel[x][y] = 1
+    for i in range(n):
+        for j in range(n):
+            if x - i == y - j:
+                sel[i][j] = 1
+            elif x + y == i + j:
+                sel[i][j] = 1
+            elif x == i or y == j:
+                sel[i][j] = 1
 
 
+sel = [[0] * 8 for _ in range(8)]
+
+search(8, 4, 4)
+
+print(sel)
