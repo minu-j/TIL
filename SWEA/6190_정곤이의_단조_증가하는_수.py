3df3cd1fd78ddf7642bfb560d6297eea61bba45b
minu-j <minu.j.dev@gmail.com>
2022-08-26 09:17:22 +0900

    0826 til

--- SWEA/6190_정곤이의_단조_증가하는_수.py
+++ SWEA/6190_정곤이의_단조_증가하는_수.py
@@ -13,14 +13,15 @@ def danjo(n, m):
 
 for tc in range(int(input())):
     N = int(input())
-    arr = sorted(list(map(int, input().split())), reverse=True)   #
-    max_danjo = -1
-
+    arr = sorted(list(map(int, input().split())), reverse=True)   # 큰 값부터 확인하도록 내림차순으로 정렬
+    max_danjo = -1      # 단조가 없다면 -1 출력되도록 초기값을 -1로 지정
+    
+    # 두 수의 곱이 단조인지 확인
     for i in range(N):
         for j in range(i + 1, N):
-            if danjo(arr[i], arr[j]):
-                if (arr[i] * arr[j]) > max_danjo:
-                    max_danjo = arr[i] * arr[j]
-                    break
+            if arr[i] * arr[j] > max_danjo:         # 두 수의 값이 지금까지 가장 큰 단조보다 크다면?
+                if danjo(arr[i], arr[j]):           # 그리고 단조라면?
+                    max_danjo = arr[i] * arr[j]     # 가장 큰 단조를 바꿔주고
+                    break                           # 해당 줄은 더이상 볼 필요가 없으므로(내림차순이므로) 다음줄로 넘어감
 
     print(f'#{tc + 1} {max_danjo}')
