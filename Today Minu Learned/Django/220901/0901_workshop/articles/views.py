38ce80373ec061ff3664642a16af86c0272ef0e7
minu-j <minu.j.dev@gmail.com>
2022-09-01 15:42:11 +0900

    0901 til

---
+++ Today Minu Learned/Django/220901/0901_workshop/articles/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+# Create your views here.
+def read(request):
+    articles = Article.objects.all()
+    context = {
+        'articles' : articles,
+    }
+    return render(request, 'articles/read.html', context)
+
+def create(request):
+    return render(request, 'articles/create.html')
+
+def post(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    article = Article(title=title, content=content)
+    article.save()
+    return redirect('articles:detail', article.pk)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:read')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
