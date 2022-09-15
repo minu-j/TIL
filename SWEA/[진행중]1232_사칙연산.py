c630d9155218e97981836161e884a865510bc45c
minu-j <minu.j.dev@gmail.com>
2022-09-15 22:43:10 +0900

    0915 til

--- SWEA/[진행중]1232_사칙연산.py
+++ SWEA/[진행중]1232_사칙연산.py
@@ -1,59 +1,54 @@
-def inorder(n):
-    if n > N:
-        return
-    inorder(n * 2)
-    inorder(n * 2 + 1)
-    order.append(n)
+# 후위순회 순서의 트리 리스트를 생성하는 함수
+def postorder(n):
+    if left[n]:
+        postorder(left[n])
+    if right[n]:
+        postorder(right[n])
+    ordered_tree.append(tree[n])
 
 
 for tc in range(10):
     N = int(input())
-    ans = 0
+    tree = [0] * (N + 1)
+    left = [0] * (N + 1)
+    right = [0] * (N + 1)
 
-    tree = [[0] * (N + 1) for _ in range(3)]
-
-    for i in range(N):
-        case_list = list(input().split())
-
-        if len(case_list) == 4:
-            tree[0][int(case_list[0])] = case_list[1]
-            tree[1][int(case_list[0])] = case_list[2]
-            tree[2][int(case_list[0])] = case_list[3]
+    # 노드의 값, 좌/우 노드 정보를 리스트에 입력
+    for _ in range(N):
+        input_list = list(input().split())
+        if len(input_list) == 4:
+            idx = int(input_list[0])
+            tree[idx] = input_list[1]
+            left[idx] = int(input_list[2])
+            right[idx] = int(input_list[3])
         else:
-            tree[0][int(case_list[0])] = case_list[1]
+            idx = int(input_list[0])
+            tree[idx] = int(input_list[1])
 
-    order = []
-    inorder(1)
+    # 입력받은 트리 리스트를 후위순회 순서로 정렬
+    ordered_tree = []
+    postorder(1)
 
+    # 후위연산
     stack = []
-
-    for j in order:
-        stack.append(tree[0][j])
-
-    print(tree)
-    print(order)
-    print(stack)
-
-    nums = []
-    for k in stack:
-        print(k, nums)
-        if k == "+":
-            B = nums.pop()
-            A = nums.pop()
-            nums.append(A + B)
-        elif k == "-":
-            B = nums.pop()
-            A = nums.pop()
-            nums.append(A - B)
-        elif k == "*":
-            B = nums.pop()
-            A = nums.pop()
-            nums.append(A * B)
-        elif k == "/":
-            B = nums.pop()
-            A = nums.pop()
-            nums.append(A / B)
+    for i in ordered_tree:
+        if i == '+':
+            B = stack.pop()
+            A = stack.pop()
+            stack.append(A + B)
+        elif i == '-':
+            B = stack.pop()
+            A = stack.pop()
+            stack.append(A - B)
+        elif i == '*':
+            B = stack.pop()
+            A = stack.pop()
+            stack.append(A * B)
+        elif i == '/':
+            B = stack.pop()
+            A = stack.pop()
+            stack.append(A / B)
         else:
-            nums.append(int(k))
+            stack.append(i)
 
-    print(f'#{tc + 1} {int(nums[-1])}')
+    print(f'#{tc + 1} {int(stack[0])}')
