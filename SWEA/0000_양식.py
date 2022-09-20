84dd68fae39ef84d231830ee55777064b57e1358
minu-j <minu.j.dev@gmail.com>
2022-09-20 12:52:44 +0900

    0920 til

--- SWEA/0000_양식.py
+++ SWEA/0000_양식.py
@@ -1,18 +1,6 @@
 for tc in range(int(input())):
     N, M = map(int, input().split())
-
-    # 16진수 암호 추출하기
-    passcode_hex_set = set()
-    for _ in range(N):
-        row = input().split('0')
-        for code in row:
-            if code:
-                passcode_hex_set.add(code)
-
-    for passcode_hex in passcode_hex_set:
-        print(passcode_hex)
-
-        for hex_num in passcode_hex:
+    matrix = [list(map(int, input().split('0'))) for _ in range(N)]
 
 
     ans = 0
