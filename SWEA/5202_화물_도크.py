6d2290bd5ae6ae72e7d0dfeff60a0cf63e140b71
minu-j <minu.j.dev@gmail.com>
2022-09-22 12:58:35 +0900

    0922 til

--- SWEA/5202_화물_도크.py
+++ SWEA/5202_화물_도크.py
@@ -1,17 +1,15 @@
 for tc in range(int(input())):
     N = int(input())
 
-    works = []
+    works = [tuple(map(int, input().split())) for i in range(N)]
+    works.sort(key=lambda x: (x[1], -x[0]))   # 종료시간이 이르고, 시작시간이 늦은 순서대로 정렬
 
-    for i in range(N):
-        works.append(tuple(map(int, input().split())))
-
-    works.sort()
-
-    select = [0] * N
+    finish = 0
     ans = 0
-    comb(0)
 
-
+    for work in works:
+        if work[0] >= finish:   # 직전 활동이 끝난 시간보다 시작시간이 이를 경우 +1
+            finish = work[1]
+            ans += 1
 
     print(f'#{tc + 1} {ans}')
