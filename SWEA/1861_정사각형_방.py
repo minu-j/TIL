fea39147251ceb3532395a96293f2811ae885cb5
minu-j <minu.j.dev@gmail.com>
2022-09-29 15:36:39 +0900

    0929 til

--- SWEA/1861_정사각형_방.py
+++ SWEA/1861_정사각형_방.py
@@ -6,6 +6,7 @@ dy = [1, 0, -1, 0]
 
 # 델타검색으로 상하좌우 값 중에서 1만큼 큰 숫자를 가지는 칸 찾기
 def move(start, n, x, y, step):
+    visited.append(n)
     for _ in range(4):
         if 0 <= x + dx[_] < N and 0 <= y + dy[_] < N and matrix[x + dx[_]][y + dy[_]] == n + 1:
             return move(start, matrix[x + dx[_]][y + dy[_]], x + dx[_], y + dy[_], step + 1)
@@ -18,10 +19,13 @@ for tc in range(int(input())):
 
     moves = []
 
+    visited = []
+
     # 2차원리스트를 순회하면서 각 항목별 이동횟수 찾기
     for i in range(N):
         for j in range(N):
-            moves.append(move(matrix[i][j], matrix[i][j], i, j, 1))
+            if matrix[i][j] not in visited:
+                moves.append(move(matrix[i][j], matrix[i][j], i, j, 1))
 
     # 정답은 이동횟수가 가장 많은 값 중에서 가장 숫자가 작은 값
     ans = max(moves, key=lambda x: (x[1], -x[0]))
