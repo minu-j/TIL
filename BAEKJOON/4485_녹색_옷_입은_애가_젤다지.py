f7ede8bbd5fbea26df464d4a68384700d9d57692
minu-j <minu.j.dev@gmail.com>
2022-09-30 15:11:54 +0900

    0930 til

--- BAEKJOON/4485_녹색_옷_입은_애가_젤다지.py
+++ BAEKJOON/4485_녹색_옷_입은_애가_젤다지.py
@@ -31,7 +31,7 @@
 #
 ################################################################
 
-# 다익스트라를 활용한 사방탐색
+다익스트라
 
 def move():
     dist = [[9999999999999999] * N for _ in range(N)]   # 맵이 2차원이니깐 dist도 2차원
@@ -59,6 +59,41 @@ def move():
                         dist[min_idx[0] + i][min_idx[1] + j] = matrix[min_idx[0] + i][min_idx[1] + j] + dist[min_idx[0]][min_idx[1]]
 
     return dist[N - 1][N - 1]   # 도착점의 dist값 출력
+
+
+tc = 0
+while True:
+    tc += 1
+    N = int(input())
+    if N == 0:
+        break
+    matrix = [list(map(int, input().split())) for _ in range(N)]   # 입력받은 matrix를 인접행렬처럼 사용
+
+    print(f'Problem {tc}: {move()}')
+
+################################################################
+
+# heapq를 이용한 다익스트라
+import heapq
+
+
+def move():
+    dist = [[9999999999999999] * N for _ in range(N)]
+    visited = set()
+    heap = []
+    heapq.heappush(heap, (matrix[0][0], (0, 0)))
+
+    while heap:
+        rupee, p = heapq.heappop(heap)
+        dist[p[0]][p[1]] = rupee
+        visited.add(p)
+
+        for i, j in (-1, 0), (0, 1), (1, 0), (0, -1):
+            if 0 <= p[0] + i < N and 0 <= p[1] + j < N:
+                if (p[0] + i, p[1] + j) not in visited and dist[p[0] + i][p[1] + j] > matrix[p[0] + i][p[1] + j] + rupee:
+                    dist[p[0] + i][p[1] + j] = matrix[p[0] + i][p[1] + j] + rupee
+                    heapq.heappush(heap, (matrix[p[0] + i][p[1] + j] + rupee, (p[0] + i, p[1] + j)))
+    return dist[N - 1][N - 1]
 
 
 tc = 0
