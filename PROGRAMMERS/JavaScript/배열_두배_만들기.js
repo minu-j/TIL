2d98974969dba987571c3c89573dd18655e82f8f
minu-j <minu.j.dev@gmail.com>
2022-10-21 17:41:31 +0900

    1021 til

---
+++ PROGRAMMERS/JavaScript/배열_두배_만들기.js
@@ -0,0 +1,4 @@
+function solution(numbers) {
+    const answer = numbers.map((num) => num * 2)
+    return answer;
+}
---
+++ PROGRAMMERS/JavaScript/아이스_아메리카노.js
@@ -0,0 +1,1 @@
+const solution = money => [parseInt(money / 5500), money - (parseInt(money / 5500) * 5500)]
