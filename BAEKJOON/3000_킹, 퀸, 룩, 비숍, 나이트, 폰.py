8371e991611bc922432bf19bbd4439e6d9cf8e76
minu-j <minu.j.dev@gmail.com>
2022-10-21 10:22:00 +0900

    1021 til

---
+++ BAEKJOON/3000_킹, 퀸, 룩, 비숍, 나이트, 폰.py
@@ -0,0 +1,2 @@
+K, Q, L, B, k, P = map(int, input().split())
+print(f'{1 - K} {1 - Q} {2 - L} {2 - B} {2 - k} {8 - P}')
--- BAEKJOON/4673_셀프_넘버.py
+++ BAEKJOON/4673_셀프_넘버.py
@@ -1,2 +1,11 @@
-def d(n):
-    
+def self_number(n):
+    num = n
+    for _ in str(n):
+        num += int(_)
+    return num
+
+
+print(self_number(33))
+print(self_number(39))
+print(self_number(51))
+print(self_number(7521))
