460bf461ca7a84cf73541ee119921687b612ad5f
minu-j <minu.j.dev@gmail.com>
2022-11-07 17:48:53 +0900

    1107 til

---
+++ Today Minu Learned/Vue.js/221107/vuex-app/src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    message: 'message in store',
+  },
+  getters: {
+  },
+  mutations: {
+    CHANGE_MESSAGE(state, newMessage) {
+      console.log(state)
+      console.log(newMessage)
+      state.message = newMessage
+    }
+  },
+  actions: {
+    changeMessage(context, newMessage) {
+      // console.log(context)
+      // console.log(newMessage)
+      context.commit('CHANGE_MESSAGE', newMessage)
+    }
+  },
+  modules: {
+  }
+})
