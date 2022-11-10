dc1780939d0143a0c0dec3e6f1df1ab7047767cd
minu-j <minu.j.dev@gmail.com>
2022-11-10 17:52:20 +0900

    1110 til

--- BAEKJOON/[진행중]1967_트리의_지름.py
+++ BAEKJOON/[진행중]1967_트리의_지름.py
@@ -1,15 +1,34 @@
 from heapq import *
 from pprint import pprint
 
+
+def ds(start):
+    dist = [99999999] * (N + 1)
+    visited = set()
+    heap = []
+    heappush(heap, (0, start))
+
+    while heap:
+        w, node = heappop(heap)
+        if node not in visited:
+            dist[node] = w
+            visited.add(node)
+
+            for destination in range(N + 1):
+                if destination not in visited and dist[destination] > matrix[node][destination] + dist[node]:
+                    heappush(heap, (matrix[node][destination] + dist[node], destination))
+    return dist
+
+
 N = int(input())
 last_child = set(range(1, N + 1))
-matrix = [[0] * (N + 1) for _ in range(N + 1)]
+matrix = [[99999999] * (N + 1) for _ in range(N + 1)]
 for _ in range(N - 1):
     S, E, W = map(int, input().split())
-    matrix[S][E] = matrix[E][S] = W
+    matrix[S][E] = W
     if S in last_child:
         last_child.remove(S)
 
-# 뒤에서부터 dp로 거리 더하고, 끝부분만 확인하기
-for node in range(N, 0, -1):
-    print(matrix[node])
+ans = 0
+
+print(ds(5))
