6371a76c724881327f0561db8e730eda76b72979
minu-j <minu.j.dev@gmail.com>
2022-12-31 23:50:22 +0900

    221231 til

--- Today Minu Learned/Others/Self_Pjt/Day05/locustfile.py
+++ Today Minu Learned/Others/Self_Pjt/Day05/locustfile.py
@@ -3,5 +3,4 @@ from locust import HttpUser, task
 class ServerTest(HttpUser):
     @task
     def api_test(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+        self.client.get("/")
