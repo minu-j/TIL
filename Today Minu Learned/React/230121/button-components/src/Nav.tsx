ea6ac933ea8ed216f653b46a3168225d39f029ba
minu-j <minu.j.dev@gmail.com>
2023-01-27 23:37:52 +0900

    220127 til

---
+++ Today Minu Learned/React/230121/button-components/src/Nav.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+function Nav() {
+
+  return (
+    <NavBg>
+
+    </NavBg>
+  );
+}
+
+export default Nav;
+
+const NavBg = styled.div`
+
+`
