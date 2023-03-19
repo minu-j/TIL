d4c9d93267b374fee8b53af1b25d0951cab67112
minu-j <minu.j.dev@gmail.com>
2023-03-19 19:51:08 +0900

    til

---
+++ Today Minu Learned/Flutter/co_cook_demo/co_cook_demo/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/minu-j/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/minu-j/Documents/TIL/Today Minu Learned/Flutter/co_cook_demo/co_cook_demo"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/minu-j/Documents/TIL/Today Minu Learned/Flutter/co_cook_demo/co_cook_demo/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/minu-j/Documents/TIL/Today Minu Learned/Flutter/co_cook_demo/co_cook_demo/.dart_tool/package_config.json"
