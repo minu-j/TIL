035ba08ce356faff7c7e9d6a633f360167ccfd7a
정민우 <minu.j.dev@gmail.com>
2023-04-14 17:23:41 +0900

    til: next.js

---
+++ Today Minu Learned/Next.js/230414/test-app/pages/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Header from "@/src/components/Top";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <Header></Header>;
+}
