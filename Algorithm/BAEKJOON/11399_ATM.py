cc0090c849e131c9a710836a47d19eb0c1d1ea2b
정민우 <minu.j.dev@gmail.com>
2023-05-31 08:54:37 +0900

    til

--- Algorithm/BAEKJOON/11399_ATM.py
+++ Algorithm/BAEKJOON/11399_ATM.py
@@ -1,6 +1,11 @@
+# 22. 11. 3. / 113112KB / 112ms / 134B
 N = int(input())
 arr = sorted(list(map(int, input().split())))
 ans = 0
 for i in range(N):
     ans += arr[-i + -1] * (i + 1)
 print(ans)
+
+# 23. 5. 30. / 118968KB / 128ms / 140B
+N, P = int(input()), sorted(map(int, input().split()))
+print(sum(list(map(lambda x: sum(x), list(map(lambda x: P[:x], range(N, -1, -1)))))))
