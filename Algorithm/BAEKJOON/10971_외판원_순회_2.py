f090a8c8f53dc75f8d8871f1dc872b4d0d84cf69
minu-j <minu.j.dev@gmail.com>
2023-07-16 21:08:46 +0900

    til

--- Algorithm/BAEKJOON/10971_외판원_순회_2.py
+++ Algorithm/BAEKJOON/10971_외판원_순회_2.py
@@ -2,17 +2,20 @@ N = int(input())
 matrix = [list(map(int, input().split())) for _ in range(N)]
 
 def start(now=0, t=0, depth=1):
-    global ans
+    global ans, mask
+
     if depth == N and matrix[now][0]:
         ans = min(ans, t + matrix[now][0])
+        return
+
     for destination in range(N):
-        if destination not in visited and matrix[now][destination]:
-            visited.add(destination)
+        if not mask & (1 << destination) and matrix[now][destination] and t + matrix[now][destination] < ans:
+            mask |= (1 << destination)
             start(destination, t + matrix[now][destination], depth + 1)
-            visited.remove(destination)
+            mask ^= (1 << destination)
     return
 
-visited, ans = {0}, 9999999999999
+mask, ans = 1, 9999999999999
 start()
 
 print(ans)
