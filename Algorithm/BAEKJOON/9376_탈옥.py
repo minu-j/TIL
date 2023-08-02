2bb2269decd40b0795e27a17ab6dab8a4ab47e56
minu-j <minu.j.dev@gmail.com>
2023-08-02 23:20:11 +0900

    til/Users/minu-j/.pyenv/versions/3.9.9/bin/python

---
+++ Algorithm/BAEKJOON/9376_탈옥.py
@@ -0,0 +1,7 @@
+from collections import deque
+from pprint import pprint
+
+for tc in range(int(input())):
+    N, M = map(int, input().split())
+    prizon = [list(input()) for _ in range(N)]
+    pprint(prizon)
